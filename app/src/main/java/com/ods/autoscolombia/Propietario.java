86abda0d1db02e8be9dc30c691b47df850e6fff1
Andrea <andrea.chaparro@est.iudigital.edu.co>
2021-11-21 21:49:36 -0500

    Cambios Iteracion2
    Se actualiza el codigo con la iteracion2

---
+++ app/src/main/java/com/ods/autoscolombia/Propietario.java
@@ -0,0 +1,14 @@
+package com.ods.autoscolombia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Propietario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_propietario);
+    }
+}
---
+++ app/src/main/java/com/ods/autoscolombia/Vehiculos.java
@@ -0,0 +1,14 @@
+package com.ods.autoscolombia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Vehiculos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehiculos);
+    }
+}
